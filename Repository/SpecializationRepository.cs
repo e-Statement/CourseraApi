2db75204ac430bd9c28ba7212b52b80fa76ed1ca
Lyalin_Artem <aalyalin22@gmail.com>
2021-11-15 12:01:28 +0500

    Fixed files unload

--- Repository/SpecializationRepository.cs
+++ Repository/SpecializationRepository.cs
@@ -3,11 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Dapper;
-using Microsoft.Data.SqlClient;
 using Microsoft.Extensions.Configuration;
+using MySql.Data.MySqlClient;
 using Server.Logic;
-using Server.Repository.Interfaces;
 using Server.Models;
+using Server.Repository.Interfaces;
 
 namespace Server.Repository 
 {
@@ -19,8 +19,8 @@ namespace Server.Repository
 
         public async Task<OperationResult<List<Specialization>>> GetByTitleAsync(string title)
         {
-            await using var connection = new SqlConnection(_dbConnection);
-            var sql = $"SELECT * FROM [{nameof(Specialization)}] WHERE {nameof(Specialization.Title)} = '{title}'";
+            await using var connection = new MySqlConnection(_dbConnection);
+            var sql = $"SELECT * FROM {nameof(Specialization)} WHERE {nameof(Specialization.Title)} = '{title}'";
             try
             {
                 var result = await connection.QueryAsync<Specialization>(sql);
