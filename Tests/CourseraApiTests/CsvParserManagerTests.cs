e1b6f0863271c55c3c1e96141206df6a9d4b28d9
elizShtol <eliz.shtol@gmail.com>
2022-01-09 22:26:32 +0500

    add tests project

---
+++ Tests/CourseraApiTests/CsvParserManagerTests.cs
@@ -0,0 +1,7 @@
+﻿namespace CourseraApiTests
+{
+    public class CsvParserManagerTests
+    {
+        
+    }
+}
---
+++ Tests/CourseraApiTests/UploadManagerTests.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace CourseraApiTests
+{
+    public class UploadManagerTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
