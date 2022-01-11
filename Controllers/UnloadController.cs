8b36289518b5e176bc5a08190a74cca4dbf111e6
azyablic <anna.zyablic@gmail.com>
2022-01-11 16:40:57 +0500

    update unload

--- Controllers/UnloadController.cs
+++ Controllers/UnloadController.cs
@@ -4,6 +4,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Server.Dto.ResponseDto;
 using Server.Managers.Interfaces;
 using Server.Settings;
 
@@ -22,59 +23,14 @@ namespace Server.Controllers
         {
             _unloadManager = unloadManager;
             _appSettings = appSettings;
-        }   
-
-        /*/// <summary>
-        /// Выгрузить из таблицы Specialization, Course в виде файла формата xlsx всех студентов и их оценки за курсы на данной специализации
-        /// </summary>
-        /// <param name="specializationName">Название специализации</param>
-        /// <returns></returns>
-        [ProducesResponseType(typeof(FileContentResult), 200)]
-        [HttpPost("specialization")]
-        public async Task<ActionResult> UnloadBySpecializationAsync([FromBody] string specializationName)
-        {
-            var result = await _unloadManager.UnloadBySpecializationAsync(specializationName);
-            if (result.IsSuccess)
-            {
-                var filePath = Path.Combine(_appSettings.Path, _appSettings.UnloadSpecializationFileName) + ".xlsx";
-                var fileName = _appSettings.UnloadSpecializationFileName + ".xlsx";
-                var fileBytes = await System.IO.File.ReadAllBytesAsync(filePath);
-                
-                System.IO.File.Delete(filePath);
-                
-                return File(fileBytes, "application/force-download", fileName);
-            }
-
-            return BadRequest();
         }
         
-        /// <summary>
-        /// Выгрузить из таблицы Course в виде файла формата xlsx всех студентов и их оценки, выбравшие данные курсы
-        /// </summary>
-        /// <param name="courses">Название курсов</param>
-        /// <returns></returns>
-        [ProducesResponseType(typeof(FileContentResult), 200)]
-        [HttpPost("courses")]
-        public async Task<ActionResult> UnloadByCoursesAsync([FromBody] List<string> courses)
+        [ProducesResponseType(typeof(GetCoursesAndSpecializationsToUnloadResponseDto), 200)]
+        [HttpPost("unload")]
+        public async Task<ActionResult> UnloadAsync([FromBody] GetCoursesAndSpecializationsToUnloadResponseDto unloadResponseDto)
         {
-            var result = await _unloadManager.UnloadByCoursesAsync(courses);
-            if (result.IsSuccess)
-            {
-                var filePath = Path.Combine(_appSettings.Path, _appSettings.UnloadCoursesFileName) + ".xlsx";
-                var fileName = _appSettings.UnloadCoursesFileName + ".xlsx";
-                var fileBytes = await System.IO.File.ReadAllBytesAsync(filePath);
-                
-                System.IO.File.Delete(filePath);
-                
-                return File(fileBytes, "application/force-download", fileName);
-            }
-
-            return BadRequest();
-        }*/
-        [ProducesResponseType(typeof(FileContentResult), 200)]
-        /*[HttpPost("courses")]*/
-        public async Task<ActionResult> UnloadAsync([FromBody] List<string> courses, [FromBody] List<string> specializationName)
-        {
+            var courses = unloadResponseDto.Courses;
+            var specializationName = unloadResponseDto.Specializations;
             var result = await _unloadManager.UnloadAsync(courses, specializationName);
             if (result.IsSuccess)
             {
--- Managers/UnloadManager.cs
+++ Managers/UnloadManager.cs
@@ -28,129 +28,16 @@ namespace Server.Managers
             _appSettings = appSettings;
         }
         
-        /*public async Task<OperationResult> UnloadBySpecializationAsync(string specializationTitle)
-        {
-            var getSpecializationsResult = await _specializationRepository.GetByTitleAsync(specializationTitle);
-            if (!getSpecializationsResult.IsSuccess)
-            {
-                return OperationResult.Error(getSpecializationsResult.ErrorText);
-            }
-
-            var specializations = getSpecializationsResult.Data;
-            var possibleCoursesCount = new Dictionary<string, int>();
-            List<string> possibleCourses;
-            foreach (var specialization in specializations)
-            {
-                var getCourses = await _courseRepository.GetBySpecializationIdAsync(specialization.Id);
-                if (getCourses.IsSuccess)
-                {
-                    foreach (var course in getCourses.Data)
-                    {
-                        if (!possibleCoursesCount.ContainsKey(course.Title))
-                        {
-                            possibleCoursesCount.Add(course.Title, 0);
-                        }
-                        possibleCoursesCount[course.Title]++;
-                    }
-                }
-            }
-
-            if (specializations.Count > 5)
-                possibleCourses = possibleCoursesCount.Where(keyValue => keyValue.Value > 5)
-                    .Select(keyValue => keyValue.Key).ToList();
-            else
-            {
-                possibleCourses = possibleCoursesCount.Select(keyValue => keyValue.Key).ToList();
-            }
-
-            var workbook = new Workbook();
-            var sheet = workbook.Worksheets[0];
-            
-            //appending header
-            var cell = sheet.Cells[0,0];
-            cell.PutValue("ФИО студента");
-            for (var i = 0; i < possibleCourses.Count; i++)
-            {
-                cell = sheet.Cells[0,i + 1];
-                cell.PutValue(possibleCourses.ElementAt(i));
-            }
-
-            var row = 1;
-            var column = 0;
-            foreach (var specialization in specializations)
-            {
-                var student = await _studentRepository.GetAsync(specialization.StudentId);
-                var courses = await _courseRepository.GetBySpecializationIdAsync(specialization.Id);
-                if (!courses.IsSuccess)
-                    continue;
-                cell = sheet.Cells[row, column];
-                cell.PutValue(student.Data.FullName);
-                column++;
-                for (int i = 0; i < possibleCourses.Count; i++)
-                {
-                    var course = courses.Data.FirstOrDefault(crs => crs.Title == possibleCourses.ElementAt(i));
-                    cell = sheet.Cells[row, column];
-                    if (course is not null)
-                    {
-                        cell.PutValue(Math.Round(course.Grade, 2));
-                    }
-                    else
-                    {
-                        cell.PutValue(0);
-                    }
-
-                    column++;
-                }
-
-                column = 0;
-                row++;
-            }
-            sheet.AutoFitColumns();
-            workbook.Save(Path.Combine(_appSettings.Path, _appSettings.UnloadSpecializationFileName) + ".xlsx");
-            return OperationResult.Success();
-        }
-
-        public async Task<OperationResult> UnloadByCoursesAsync(List<string> courseTitles)
-        {
-            var workbook = new Workbook();
-            var sheet = workbook.Worksheets[0];
-            var row = 0;
-            var column = 0;
-            foreach (var courseTitle in courseTitles)
-            {
-                var getCourses = await _courseRepository.GetByTitleAsync(courseTitle);
-                if (!getCourses.IsSuccess)
-                {
-                    return OperationResult.Error("Ошибка при получении курса с названием " + courseTitle);
-                }
-
-                var courses = getCourses.Data;
-                sheet.Cells[row, column].PutValue("ФИО студента");
-                sheet.Cells[row, column + 1].PutValue(courseTitle);
-                
-                foreach (var course in courses)
-                {
-                    var student = await _studentRepository.GetAsync(course.StudentId);
-                    row += 1;
-                    column = 0;
-                    sheet.Cells[row,column].PutValue(student.Data.FullName);
-                    sheet.Cells[row, column + 1].PutValue(Math.Round(course.Grade, 2));
-                }
-
-                row += 2;
-            }
-            sheet.AutoFitColumns();
-            workbook.Save(Path.Combine(_appSettings.Path, _appSettings.UnloadCoursesFileName) + ".xlsx");
-            return OperationResult.Success();
-        }*/
         public async Task<OperationResult> UnloadAsync(List<string> courses, List<string> specializationTitles)
         {
+            var coursesWorksheetAppender =
+                new CoursesWorksheetAppender(_courseRepository, _studentRepository, _appSettings);
+            var specializationWorksheetAppender = new SpecializationWorksheetAppender(_courseRepository,
+                _studentRepository, _specializationRepository, _appSettings);
             var workbook = new Workbook();
-            CoursesWorksheetAppender.Append(workbook, courses);
+            await coursesWorksheetAppender.Append(workbook, courses);
             foreach (var specializationTitle in specializationTitles)
-            {
-                SpecializationWorksheetAppender.Append(workbook, specializationTitle);
-            }
+                await specializationWorksheetAppender.Append(workbook, specializationTitle);
             workbook.Save(Path.Combine(_appSettings.Path, _appSettings.UnloadCoursesFileName) + ".xlsx");
             return OperationResult.Success();
         }
